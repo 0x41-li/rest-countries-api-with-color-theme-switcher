0d1db0fd089cdd07c6062a34736502e3e92243d6
Ali Sbane <ali.sbane.dev@gmail.com>
2022-10-08 17:41:43 +0100

    Saving Fectched coutnries Data in localStorage for future usage

--- src/contexts/CountriesContext.jsx
+++ src/contexts/CountriesContext.jsx
@@ -1,19 +1,32 @@
-import { createContext, useState } from "react";
+import { createContext, useEffect, useState } from "react";
 
 export const CountriesContext = createContext({});
+
 export const CountriesContextProvider = (props) => {
-  const [countries, setCountries] = useState({});
+  const savedCountriesAsString =
+    localStorage.getItem("countries_data") ?? undefined;
+  const savedCountriesAsJson = savedCountriesAsString
+    ? JSON.parse(savedCountriesAsString)
+    : {};
 
-  const fetchCountries = async () => {
-    const resp = await fetch("https://restcountries.com/v3.1/all");
-    const respJson = await resp.json();
-    setCountries(respJson);
-  };
+  const [countriesData, setCountries] = useState(savedCountriesAsJson);
 
-  fetchCountries();
+  useEffect(() => {
+    if (savedCountriesAsString === undefined) {
+      const fetchCountries = async () => {
+        const resp = await fetch("https://restcountries.com/v3.1/all");
+        const respJson = await resp.json();
+        setCountries(respJson);
+      };
+
+      fetchCountries();
+    }
+
+    localStorage.setItem("countries_data", JSON.stringify(countriesData));
+  }, [countriesData]);
 
   return (
-    <CountriesContext.Provider value={countries}>
+    <CountriesContext.Provider value={countriesData}>
       {props.children}
     </CountriesContext.Provider>
   );
