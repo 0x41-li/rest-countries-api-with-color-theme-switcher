e4a415965cad88bc0332cd564471f1844a154f1f
Ali Sbane <ali.sbane.dev@gmail.com>
2022-11-12 10:32:30 +0100

    So that padding is subtracted from the containers

--- src/sections/Header.jsx
+++ src/sections/Header.jsx
@@ -10,15 +10,13 @@ const Header = () => {
   return (
     <header data-dark-theme={isDarkTheme.toString()}>
       <div className="container">
-        <div className="header__inner">
-          <h1>Where in the world?</h1>
-          <button className="header__toggle-theme-btn" onClick={toggleTheme}>
-            <div className="header__toggle-theme-btn__moon-icon">
-              <MoonIcon />
-            </div>
-            <p className="header__toggle-theme-btn__para">Dark Mode</p>
-          </button>
-        </div>
+        <h1>Where in the world?</h1>
+        <button className="header__toggle-theme-btn" onClick={toggleTheme}>
+          <div className="header__toggle-theme-btn__moon-icon">
+            <MoonIcon />
+          </div>
+          <p className="header__toggle-theme-btn__para">Dark Mode</p>
+        </button>
       </div>
     </header>
   );
